9c3cb4b77fbce5345c29669961e5ac7eace57d99
bbditer <maxleoleidl@gmail.com>
2017-09-29 18:59:35 +0200

    now, create the menu with handlebars and node.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,27 @@
 var express = require('express');
 var router = express.Router();
-// var fs = require('fs');
 
-// var data = fs.readFileSync('public/sources/opening_times.json', 'utf8');
-var data = require('../public/sources/opening_times');
+var menu_data = require('../public/sources/menu');
+var opening_data = require('../public/sources/opening_times');
+
+var activelink = 'Praxis';
+
+var x = 0;
+while (x < menu_data.entries.length) {
+  if (menu_data.entries[x].title == activelink) {
+    if (menu_data.entries[x].class == undefined) {
+      menu_data.entries[x].class = 'menuactive';
+    } else {
+      menu_data.entries[x].class += ' menuactive';
+    };
+  };
+
+  x++;
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Schauenburger Hausarztpraxis', opening_times: data });
+  res.render('index', { title: 'Schauenburger Hausarztpraxis', menu: menu_data, opening_times: opening_data });
 });
 
 module.exports = router;
