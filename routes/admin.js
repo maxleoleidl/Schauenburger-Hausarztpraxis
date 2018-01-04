d181ef117e07643cb870331ec9050b824f589d0f
bbditer <maxleoleidl@gmail.com>
2018-01-04 19:09:37 +0100

    First success to change data from the news.json file

--- routes/admin.js
+++ routes/admin.js
@@ -11,9 +11,53 @@ var practice_data = require('../public/sources/practice_team');
 var services_data = require('../public/sources/services');
 var contact_data = require('../public/sources/contact');
 
+var data_commit = news_data.entries;
+
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('admin', {title: "Administration"});
+    res.render('admin', {title: "Administration", data: data_commit});
+});
+
+router.post('/submit', function(req, res, next) {
+    /* backup the source files */
+    fs.readFile('public/sources/news.json', 'utf8', function(err, data) {
+        fs.writeFileSync('public/backup/news.json', data);
+    });
+
+    /* read data */
+    var headline = req.body.headline;
+    var items = [];
+    var i = 0;
+
+    while (i < 100) {
+        try {
+            var item = eval('req.body.item_' + i);
+            if (item != null) {
+                items.push.apply(items, [{"id": i, "entry": item}]);
+            };
+        } catch(err) {
+            i = 100;
+        };
+
+        i++;
+    };
+
+    /* write data in the right form */
+    var output = JSON.stringify({
+        "content": true,
+        "entries":
+        [
+            {
+                "headline": headline,
+                "items": items
+            }
+        ]
+    });
+
+    /* write data to file */
+    fs.writeFileSync('public/sources/news.json', output);
+
+    res.redirect('/admin');
 });
 
 module.exports = router;
