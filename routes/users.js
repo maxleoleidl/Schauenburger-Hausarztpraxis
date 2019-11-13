27734dcf35b222bb41e60629cfa1431982db72ba
bbditer <maxleoleidl@gmail.com>
2019-11-13 09:38:15 +0100

    Changes some images and the news.

--- routes/users.js
+++ routes/users.js
@@ -5,48 +5,8 @@ var LocalStrategy = require('passport-local').Strategy;
 
 var User = require('../models/user');
 
-router.get('/register', function(req, res) {
-    res.render('register');
-});
-
 router.get('/login', function(req, res) {
     res.render('login');
-});
-
-// delete this section on the server
-router.post('/register', function(req, res) {
-    var name = req.body.name;
-    var username = req.body.username;
-    var password = req.body.password;
-    var password2 = req.body.password2;
-
-    //Validation
-    req.checkBody('name', 'Bitte geben sie einen Namen an.').notEmpty();
-    req.checkBody('username', 'Bitte geben sie einen Benutzernamen an').notEmpty();
-    req.checkBody('password', 'Bitte geben sie ein Passwort ein.').notEmpty();
-    req.checkBody('password2', 'Die von ihnen eingegebenen Passwörter stimmen nicht überein.').equals(req.body.password);
-
-    var errors = req.validationErrors();
-
-    if(errors) {
-        res.render('register', {
-            errors:errors
-        });
-    } else {
-        var newUser = new User({
-            name: name,
-            username: username,
-            password: password
-        });
-
-        User.createUser(newUser, function(err, user) {
-            if(err) {throw err};
-        });
-
-        req.flash('success_msg', 'Der Benutzer wurde erfolgreich angelegt. Sie können sich jetzt anmelden.');
-
-        res.redirect('/users/login');
-    };
 });
 
 passport.use(new LocalStrategy(function(username, password, done) {
